74067d4dbd603020c3cb713ba42f3ad31522a105
Mariem Tijani <mariemtijani60@gmail.com>
2025-07-13 23:13:09 +0100

    fc

--- properties/utils.py
+++ properties/utils.py
@@ -1,5 +1,9 @@
+import logging
 from django.core.cache import cache
 from .models import Property
+from django_redis import get_redis_connection
+
+logger = logging.getLogger(__name__)
 
 def get_all_properties():
     # Try to get data from cache
@@ -12,3 +16,23 @@ def get_all_properties():
         cache.set('all_properties', properties, 3600)
 
     return properties
+
+def get_redis_cache_metrics():
+    redis_conn = get_redis_connection("default")
+    info = redis_conn.info()
+
+    hits = info.get("keyspace_hits", 0)
+    misses = info.get("keyspace_misses", 0)
+    total = hits + misses
+
+    hit_ratio = hits / total if total > 0 else 0.0
+
+    metrics = {
+        "hits": hits,
+        "misses": misses,
+        "hit_ratio": hit_ratio
+    }
+
+    logger.info("Redis Cache Metrics: %s", metrics)
+
+    return metrics
